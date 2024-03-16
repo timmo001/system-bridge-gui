03e3a85b51db51fcbc1cf004f840680b8434553f
Aidan Timson <aidan@timmo.dev>
2024-03-16 23:54:06 +0000

    Tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,14 @@
 
 use std::error::Error;
 
-use tauri::App;
+use tauri::{
+    image::Image, menu::{MenuBuilder, MenuItemBuilder, PredefinedMenuItem}, tray::{ClickType, TrayIconBuilder}, utils::config::AppImageConfig, Manager
+};
+use tauri::{App, AppHandle};
 use tauri::{WebviewUrl, WebviewWindowBuilder};
 use tauri_plugin_autostart::MacosLauncher;
 use tauri_plugin_autostart::ManagerExt;
+// use tauri_plugin_updater::UpdaterExt;
 use tokio;
 
 // TODO: Add a way to close the backend server when the app is closed
@@ -34,6 +38,29 @@ const BACKEND_HOST: &str = "127.0.0.1";
 
 const WINDOW_WIDTH: f64 = 1280.0;
 const WINDOW_HEIGHT: f64 = 720.0;
+
+fn page_title_map() -> Vec<(&'static str, &'static str)> {
+    vec![("data", "Data"), ("settings", "Settings")]
+}
+
+fn get_settings() -> Result<Settings, Box<dyn std::error::Error>> {
+    // Get install directory from &localappdata%\timmo001\systembridge
+    let install_path: String = format!(
+        "{}/timmo001/systembridge",
+        std::env::var("LOCALAPPDATA").unwrap()
+    );
+
+    // Read settings from {install_path}\settings.json
+    let settings_path: String = format!("{}/settings.json", install_path);
+    if !std::path::Path::new(&settings_path).exists() {
+        return Err("Settings file not found".into());
+    }
+
+    let settings: String = std::fs::read_to_string(settings_path)?;
+    let settings: Settings = serde_json::from_str(&settings)?;
+
+    Ok(settings)
+}
 
 fn setup_autostart(app: &mut App, autostart: bool) -> Result<(), Box<dyn std::error::Error>> {
     println!("Autostart: {}", autostart);
@@ -105,6 +132,41 @@ async fn check_backend_api(
     Ok(())
 }
 
+fn create_window(app: &AppHandle, page: String) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Creating window: {}", page);
+
+    // Destroy the window if it already exists
+    app.get_webview_window("main").map(|window| window.close());
+
+    let settings: Settings = get_settings().unwrap();
+
+    let url: tauri::Url = format!(
+        "http://{}:{}/app/{}.html?apiPort={}&token={}",
+        BACKEND_HOST,
+        settings.api.port.to_string().clone(),
+        page,
+        settings.api.port.clone(),
+        settings.api.token.clone()
+    )
+    .parse()
+    .unwrap();
+
+    WebviewWindowBuilder::new(app, "main".to_string(), WebviewUrl::External(url))
+        .inner_size(WINDOW_WIDTH, WINDOW_HEIGHT)
+        .title(format!(
+            "{} | System Bridge",
+            page_title_map()
+                .iter()
+                .find(|(key, _)| key == &page)
+                .unwrap()
+                .1
+        ))
+        .build()
+        .unwrap();
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     // Get install directory from &localappdata%\timmo001\systembridge
@@ -120,8 +182,8 @@ async fn main() {
         std::process::exit(1);
     }
 
-    let settings: String = std::fs::read_to_string(settings_path).unwrap();
-    let settings: Settings = serde_json::from_str(&settings).unwrap();
+    // Get settings
+    let settings: Settings = get_settings().unwrap();
 
     let base_url: String = format!(
         "http://{}:{}",
@@ -145,30 +207,92 @@ async fn main() {
         std::process::exit(1);
     }
 
+    // Create the main window
     tauri::Builder::default()
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
             Some(vec![]),
         ))
-        // .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_shell::init())
+        // .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(move |app: &mut App| {
-            // Setup autostart from settings
-            setup_autostart(app, settings.autostart).unwrap();
+            // Check for updates
+            // let handle: &tauri::AppHandle = app.handle();
+            // tauri::async_runtime::spawn(async move {
+            //     let response: Result<
+            //         Option<tauri_plugin_updater::Update>,
+            //         tauri_plugin_updater::Error,
+            //     > = handle.updater().expect("REASON").check().await;
+            //     if response.is_ok() {
+            //         let update: Option<tauri_plugin_updater::Update> = response.unwrap();
+            //         if update.is_some() {
+            //             let update: tauri_plugin_updater::Update = update.unwrap();
+            //             println!("Update available: {}", update.version);
+            //         }
+            //     }
+            // });
 
-            let url: tauri::Url = format!(
-                "{}/app/{}.html?apiPort={}&token={}",
-                base_url,
-                "data",
-                settings.api.port.clone(),
-                settings.api.token.clone()
-            )
-            .parse()
-            .unwrap();
-            WebviewWindowBuilder::new(app, "main".to_string(), WebviewUrl::External(url))
-                .inner_size(WINDOW_WIDTH, WINDOW_HEIGHT)
-                .title("Data | System Bridge")
+            // Setup autostart from settings
+            setup_autostart(app, settings.autostart.clone()).unwrap();
+
+            // Setup the tray menu
+            let separator = PredefinedMenuItem::separator(app)?;
+            let settings = MenuItemBuilder::with_id("show_settings", "Open Settings")
+                .accelerator("S")
+                .build(app)?;
+            let data = MenuItemBuilder::with_id("show_data", "View Data")
+                .accelerator("D")
+                .build(app)?;
+            let check_for_updates =
+                MenuItemBuilder::with_id("check_for_updates", "Check for Updates")
+                    .accelerator("CmdOrControl+U")
+                    .build(app)?;
+            let exit = PredefinedMenuItem::quit(app, Some("Exit"))?;
+
+            let menu = MenuBuilder::new(app)
+                .items(&[
+                    &settings,
+                    &data,
+                    &separator,
+                    &check_for_updates,
+                    &separator,
+                    &exit,
+                ])
                 .build()?;
+
+            // let icon: Image = Image::
+
+            // Setup the tray icon
+            TrayIconBuilder::new()
+                .tooltip("System Bridge")
+                // .icon(icon)
+                .menu(&menu)
+                .menu_on_left_click(true)
+                .on_menu_event(
+                    move |app: &tauri::AppHandle, event: tauri::menu::MenuEvent| match event
+                        .id()
+                        .as_ref()
+                    {
+                        "show_settings" => {
+                            create_window(app, "settings".to_string()).unwrap();
+                        }
+                        "show_data" => {
+                            create_window(app, "data".to_string()).unwrap();
+                        }
+                        _ => (),
+                    },
+                )
+                .on_tray_icon_event(|tray, event| match event.click_type {
+                    ClickType::Double => {
+                        let app = tray.app_handle();
+
+                        create_window(app, "data".to_string()).unwrap();
+                    }
+                    _ => (),
+                })
+                .build(app)?;
+
             Ok(())
         })
         .run(tauri::generate_context!())
